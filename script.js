f5cb96dcdf8c98181dde1c3cb91b41a36123a852
Hawazin Abdullah <abdullah.hawazin@gmail.com>
2018-09-20 21:02:46 -0400

    Please enter the commit message for your changes. Lines starting

---
+++ script.js
@@ -0,0 +1,16 @@
+var two = 2;
+var oneFourth ="1/4";
+
+function marinaraSauce(cookName)
+{
+    console.log (`Hey, ${cookName} ! This is my marinarar Sauce recipe!`);
+    console.log (`-Heat a mediume-large sauce pan over mediume heat`);
+    console.log (`-Add  ${two} tspns of oil and 5 garlic cloves,cook until golden, about 2 minutes`);
+    console.log (`-Add  ${oneFourth} cup water,${two} cans of crushed tomatos, and salt an pepper to taste`);
+    console.log (`-Cover the pot, bring to boil reduce the heat to medium low, and simmer until the sauce heated, about 10 minutes`);
+    console.log (`-Stir in ${oneFourth} cup roughly chopped fresh basil, salt an pepper ans needed`);
+    
+
+}
+
+marinaraSauce ("Adam");
